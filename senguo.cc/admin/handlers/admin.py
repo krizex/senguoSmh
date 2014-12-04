68bfce3ac56a779d8255b8e8c7faf706d9c3aa8e
senguo2014 <senguo2014@163.com>
2014-12-04 10:48:21 +0800

    merge2

--- senguo.cc/admin/handlers/admin.py
+++ senguo.cc/admin/handlers/admin.py
@@ -252,9 +252,6 @@ class Config(AdminBaseHandler):
     def get(self, id): #shop_id
         try:config = self.session.query(models.Config).filter_by(id=id).one()
         except:return self.send_error(404)
-<<<<<<< HEAD
-        return self.render("shop-set.html", config=config,context=dict(subpage="shop_set")
-=======
         action = self.args["action"]
         if action == "delivery":
             return self.render("", addresses=config.addresses)
@@ -292,11 +289,7 @@ class Config(AdminBaseHandler):
             elif action == "edit_hire":
                 config.update(session=self.session, hire_text=data)
         elif action == "add_addr2": #id: addr1_id
-<<<<<<< HEAD
             try:addr1 = self.session.query(models.Address1).filter_by(id=id).one()
-=======
-            try: addr1 = self.session.query(models.Address1).filter_by(id=id).one()
->>>>>>> a0489f8251137c0c493ce8e6e3cb3230d7ae340d
             except:return self.send_error(404)
             addr2 = models.Address2(name=data)
             addr1.address2.append(addr2)
