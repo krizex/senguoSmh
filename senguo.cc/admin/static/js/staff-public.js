c06622201ee833a859728c039b3409db1361cce5
senguo2014 <senguo2014@163.com>
2014-12-25 15:01:40 +0800

    商品列表样式

--- senguo.cc/admin/static/js/staff-public.js
+++ senguo.cc/admin/static/js/staff-public.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
     $('.shop-list').find('li').on('click',function(){
         var id=$(this).data('id');
         shopChange(id);
-    })
-
+    });
 });
 
 function shopChange(id){
@@ -12,10 +11,20 @@ function shopChange(id){
     $.postJson(url,args,function(res){
         if(res.success){
             $('shop-list-box').modal('hide');
-            window.location.reload();
+            //window.location.reload();
         }
         else return alert(res.error_text)
     },function(){
         return alert('网络错误！')
     })
+}
+
+function job(target,n){
+    console.log(2222);
+    switch(n) {
+        case 1 :target.text('捡货员');break;
+        case 2 :target.text('送货员');break;
+        case 3 :target.text('送货员');break;
+        default :target.text('送货员');break;
+    }
 }
