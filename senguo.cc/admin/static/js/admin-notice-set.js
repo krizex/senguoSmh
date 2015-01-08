ad34f52e26af37e36066aa0a15c9b1b0b369a400
senguo2014 <senguo2014@163.com>
2015-01-08 11:20:30 +0800

    公告启用/停用

--- senguo.cc/admin/static/js/admin-notice-set.js
+++ senguo.cc/admin/static/js/admin-notice-set.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
     $('.add-new-notice').on('click',function(){
         noticeAdd();
     });
+    $('.notice_active').each(function(){
+        var $this=$(this);
+        var status=$this.data('status');
+        switch (status){
+            case status=1:$this.find('.work-mode').show().siblings('.stop-mode').hide();break;
+            case status=2:$this.find('.work-mode').hide().siblings('.stop-mode').show();break;
+        }
+    });
+    $('.notice_active').on('click',function(){
+        noticeActive($(this));
+    });
 });
 function noticeAdd(){
     var url=link;
@@ -22,6 +33,29 @@ function noticeAdd(){
         function(res){
             if(res.success){
                 $('#noticeBox').modal('hide');
+            }
+        })
+}
+
+function noticeActive(target){
+    var url=link;
+    var action="edit_notice_active";
+    var notice_id=target.parents('.set-list-item').data('id');
+    var status=target.data('status');
+    var data={
+        notice_id:notice_id
+    };
+    var args={
+        action:action,
+        data:data
+    };
+    $.postJson(url,args,
+        function(res){
+            if(res.success){
+                if(status==1){
+                    target.find('.work-mode').hide().siblings('.stop-mode').show();
+                }
+                else target.find('.work-mode').show().siblings('.stop-mode').hide();
             }
         })
 }
