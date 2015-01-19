68115942b14093b97cae2be81af116ad5963be7d
senguo2014 <senguo2014@163.com>
2015-01-19 16:15:54 +0800

    fix

--- senguo.cc/admin/static/js/market-img-fix.js
+++ senguo.cc/admin/static/js/market-img-fix.js
@@ -1,29 +1,15 @@
-var s_height;
-var w_height=window.screen.height;
-var s_width=window.screen.width;
-if(s_width<=800)
-    s_height=w_height/9;
-else  s_height=800/9;
-
-function fiximg(a,b,c,d) {
-    var item=document.getElementsByClassName(a);
-    var c1=document.getElementsByClassName(b);
-    var c2=document.getElementsByClassName(c);
-    for (var i = 0; i < item.length; i++) {
-        item[i].style.width = s_height + 'px';
-        item[i].style.height = s_height + 'px';
-        c1[i].style.width = s_height + 'px';
-        c1[i].style.height = s_height + 'px';
-        c2[i].style.height = s_height + 'px';
-    }
-}
-
-function fixclass(d){
-    var c3=document.getElementsByClassName(d);
-    for (var i = 0; i < c3.length; i++) {
-        var num=c3[i].innerHTML.length;
-        c3[i].style.height = s_height + 'px';
-        c3[i].style.lineHeight = s_height/num + 'px';
+function fixclass(t1,t2,t3,t4){
+    var item1=document.getElementsByClassName(t1);
+    var item2=document.getElementsByClassName(t2);
+    var item3=document.getElementsByClassName(t3);
+    var item4=document.getElementsByClassName(t4);
+    for (var i = 0; i < item1.length; i++) {
+        var width=item3[i].clientWidth;
+        var inner=item4[i].clientHeight;
+        item1[i].style.height=(width+12)+'px';
+        item4[i].style.marginTop=(width-inner)/2+'px';
+        item2[i].style.height=width/2+'px';
+        item2[i].style.lineHeight=width/2+'px';
     }
 }
 
