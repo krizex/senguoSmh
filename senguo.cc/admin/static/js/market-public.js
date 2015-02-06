cdfc9464846f5ab91c00cd6d1ce51fa99270d414
senguo2014 <senguo2014@163.com>
2015-02-06 16:26:01 +0800

    fix

--- senguo.cc/admin/static/js/market-public.js
+++ senguo.cc/admin/static/js/market-public.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
     $('.staff_href').attr({'href':staff_href+Int(shop_id)});
 
     //微信Api
+    wexin();
     wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: 'wx0ed17cdc9020a96e', // 必填，公众号的唯一标识
