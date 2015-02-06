7af2fdcdfa8d48341c6f6c8822ed4f54127657b6
senguo2014 <senguo2014@163.com>
2015-02-06 16:22:08 +0800

    分享到朋友圈

--- senguo.cc/admin/static/js/market-public.js
+++ senguo.cc/admin/static/js/market-public.js
@@ -17,7 +17,36 @@ $(document).ready(function(){
     }
     //$('.shop_href').attr({'href':shop_href+Int(shop_id)});
     $('.staff_href').attr({'href':staff_href+Int(shop_id)});
+
     //微信Api
+    wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: 'wx0ed17cdc9020a96e', // 必填，公众号的唯一标识
+        timestamp:timestamp_val, // 必填，生成签名的时间戳
+        nonceStr:noncestr_val, // 必填，生成签名的随机串
+        signature:signature_val,// 必填，签名，见附录1
+        jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    });
+    wx.ready(function(){
+        wx.onMenuShareTimeline({
+            title: '哈哈哈哈哈'+'一家不错的水果O2O店铺，快来关注吧~ ', // 分享标题
+            link: '', // 分享链接
+            imgUrl: '', // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+        wx.onMenuShareAppMessage({
+            title: '哈哈哈哈哈', // 分享标题
+            desc: "一家不错的水果O2O店铺，快来关注吧~ ", // 分享描述
+            link:"",
+            imgUrl: "", // 分享图标
+            type: '' // 分享类型,music、video或link，不填默认为link
+        });
+    });
 });
 var shop_href='/customer/shopProfile';
 var market_href='/shop/none';
@@ -25,6 +54,9 @@ var home_href='/customer';
 var success_href='/notice/success';
 var staff_href='/staff/hire/';
 var shop_id;
+var noncestr_val;
+var timestamp_val;
+var signature_val;
 
 function unitText(target,n){
     switch (n){
@@ -76,3 +108,18 @@ function tagText(target,n){
         };
     })(Hammer.Manager.prototype.emit);
 }));
+
+function wexin(){
+    var url='/wexin';
+    var link= window.location.href;
+    var args={url: link};
+    $.ajaxSetup({'async':false});
+    $.postJson(url,args,function(res){
+        if(res.success){
+            noncestr_val=res.noncestr;
+            timestamp_val=res.timestamp;
+            signature_val=res.signature;
+        }
+        else return alert(res.error_text);
+    })
+}
