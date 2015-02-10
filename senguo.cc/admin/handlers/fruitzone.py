1a0cd18e17e75ad9695a4ba9576a9d43abd6d87d
monklof <monklof@gmail.com>
2015-02-10 10:27:12 +0800

    七牛云回调bug，access_token获取bug

--- senguo.cc/admin/handlers/fruitzone.py
+++ senguo.cc/admin/handlers/fruitzone.py
@@ -396,7 +396,7 @@ class QiniuCallback(FruitzoneBaseHandler):
             if mgoods.img_url:  #先要把旧的的图片删除
                 m = BucketManager(auth=qiniu.Auth(ACCESS_KEY,SECRET_KEY))
                 m.delete(bucket=BUCKET_SHOP_IMG, key=mgoods.img_url.split('/')[3])
-            fruit.update(session=self.session, img_url=SHOP_IMG_HOST+key)
+            mgoods.update(session=self.session, img_url=SHOP_IMG_HOST+key)
             return self.send_success()
         elif action == "add":  #什么都不用做
             return self.send_success()
