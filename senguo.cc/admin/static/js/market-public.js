623ce831d73f8a19ee34fb01bf47507acb4ba5a2
wdg8106 <1499728038@qq.com>
2015-03-25 17:26:38 +0800

    debug off

--- senguo.cc/admin/static/js/market-public.js
+++ senguo.cc/admin/static/js/market-public.js
@@ -71,7 +71,7 @@ function wexin(){
             var timestamp_val=res.timestamp;
             var signature_val=res.signature;
             wx.config({
-             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
              appId: 'wx0ed17cdc9020a96e', // 必填，公众号的唯一标识
              timestamp:timestamp_val, // 必填，生成签名的时间戳
              nonceStr:noncestr_val, // 必填，生成签名的随机串
