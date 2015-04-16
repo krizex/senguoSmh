c389e94919165c101c8557ea2ebeafa13e298433
senguo2014 <senguo2014@163.com>
2015-04-16 19:34:27 +0800

    禁用分享test4

--- senguo.cc/admin/static/js/market-public.js
+++ senguo.cc/admin/static/js/market-public.js
@@ -81,14 +81,15 @@ function wexin(link,imgurl){
             var signature_val=res.signature;
             var logo_Item=$('#shop_imgurl');
             wx.config({
-             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
              appId: 'wx0ed17cdc9020a96e', // 必填，公众号的唯一标识
              timestamp:timestamp_val, // 必填，生成签名的时间戳
              nonceStr:noncestr_val, // 必填，生成签名的随机串
              signature:signature_val,// 必填，签名，见附录1
-             jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','hideMenuItems'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+             jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','hideMenuItems','hideOptionMenu','showOptionMenu']// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
          });
          wx.ready(function(){
+            wx.hideOptionMenu();
              if(logo_Item.length==0){
                 wx.hideMenuItems({
                     menuList: ['menuItem:share:appMessage','menuItem:share:timeline'] 
