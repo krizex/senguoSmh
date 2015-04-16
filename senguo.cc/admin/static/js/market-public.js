ef63135fa2d178c99627f9328790023b2b5672b8
senguo2014 <senguo2014@163.com>
2015-04-16 19:38:21 +0800

    禁用分享test5

--- senguo.cc/admin/static/js/market-public.js
+++ senguo.cc/admin/static/js/market-public.js
@@ -81,19 +81,23 @@ function wexin(link,imgurl){
             var signature_val=res.signature;
             var logo_Item=$('#shop_imgurl');
             wx.config({
-             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
              appId: 'wx0ed17cdc9020a96e', // 必填，公众号的唯一标识
              timestamp:timestamp_val, // 必填，生成签名的时间戳
              nonceStr:noncestr_val, // 必填，生成签名的随机串
              signature:signature_val,// 必填，签名，见附录1
-             jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','hideMenuItems','hideOptionMenu','showOptionMenu']// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+             jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','hideMenuItems','hideOptionMenu','showOptionMenu'],// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            success:function(){
+                            wx.hideOptionMenu();
+            }
          });
          wx.ready(function(){
-            wx.hideOptionMenu();
+
              if(logo_Item.length==0){
                 wx.hideMenuItems({
                     menuList: ['menuItem:share:appMessage','menuItem:share:timeline'] 
                 });
+                wx.showOptionMenu();
              }
             wx.onMenuShareTimeline({
              title: '', // 分享标题
