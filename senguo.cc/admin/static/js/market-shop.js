72b3993821b469a6d858c84968029fc5ee8ab0a3
zhoubing <418650386@qq.com>
2015-04-29 16:30:56 +0800

    update

--- senguo.cc/admin/static/js/market-shop.js
+++ senguo.cc/admin/static/js/market-shop.js
@@ -17,6 +17,8 @@ $(document).ready(function(){
     signin($this);
 }).on('click','.signed',function(){
     noticeBox('亲，你今天已经签到了，一天只能签到一次哦')
+}).on("click","#show-more",function(){
+    window.location.href=$(this).find("a").attr("href");
 });
 function focus(){
     var url='';
