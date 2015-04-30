d8b75c6b62f6d7375def9fd74b9a675f96d33999
wdg8106 <1499728038@qq.com>
2015-04-30 23:44:11 +0800

    get_cookie

--- senguo.cc/admin/handlers/fruitzone.py
+++ senguo.cc/admin/handlers/fruitzone.py
@@ -748,7 +748,8 @@ class payTest(FruitzoneBaseHandler):
 	def get(self):
 		print(self.request.full_url())
 		path_url = self.request.full_url()
-		totalPrice = self.args['totalPrice']
+		# totalPrice = self.args['totalPrice']
+
 		jsApi  = JsApi_pub()
 		#path = 'http://auth.senguo.cc/fruitzone/paytest'
 		path = APP_OAUTH_CALLBACK_URL + self.reverse_url('fruitzonePayTest')
@@ -769,7 +770,9 @@ class payTest(FruitzoneBaseHandler):
 				print('openid not exit')
 			
 			unifiedOrder =   UnifiedOrder_pub()
-			totalPrice = self.args['totalPrice'] 
+			# totalPrice = self.args['totalPrice'] 
+			totalPrice = self.get_cookie('money')
+			print(totalPrice,'long time no see!')
 			unifiedOrder.setParameter("body",'senguo')
 			unifiedOrder.setParameter("notify_url",'http://zone.senguo.cc/callback')
 			unifiedOrder.setParameter("openid",openid.encode('utf-8'))
