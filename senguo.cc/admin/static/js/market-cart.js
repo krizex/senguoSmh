65c7ae80f6d0f689a6ec3a26f2dd46cd90a8c1d4
zhoubing <418650386@qq.com>
2015-05-18 20:46:18 +0800

    order detail

--- senguo.cc/admin/static/js/market-cart.js
+++ senguo.cc/admin/static/js/market-cart.js
@@ -368,12 +368,10 @@ $(document).ready(function(){
     if(index != 2){
         $(".wrap-online-lst").addClass("hidden");
     }
-    if(index == 1){
-        var statu = $(this).attr("data-auth");
-        if(statu == "False"){
-            noticeBox("当前店铺未认证，此功能暂不可用");
-            return false;
-        }
+    var statu = $(this).attr("data-auth");
+    if(statu == "False"){
+        noticeBox("当前店铺未认证，此功能暂不可用");
+        return false;
     }
     if(status==0){
         noticeBox("当前店铺已关闭"+type);
@@ -382,7 +380,6 @@ $(document).ready(function(){
     if(index == 2){
         $(".wrap-online-lst").toggleClass("hidden");
     }
-
     $(".pay_type li").removeClass("active").eq(index).addClass("active");
 }).on('click','.a-cz',function(){
     var status = $(this).attr('data-status');
