7e0f63a685b011eb39ddade0e569e4b28116634e
Sky <littlesky87@gmail.com>
2015-05-25 13:02:45 +0800

    fadeOut test

--- senguo.cc/admin/static/js/market-public.js
+++ senguo.cc/admin/static/js/market-public.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+ $(document).ready(function(){
     window.dataObj.shop_href='/customer/shopProfile';
     window.dataObj.market_href='/shop/none';
     window.dataObj.home_href='/customer';
@@ -338,19 +338,19 @@ Modal.prototype.modal=function(type){
     {
         var window_height=$(window).height();
         var height=$('.container').height();
-        $target.removeClass('fade').addClass('in').css({'display':'block'});
+        $target.removeClass('anim-fadeOut')//.addClass('in').css({'display':'block'});
         $target.find('.warn').remove();
         $("body").css({'overflow':'hidden'});
         $target.on('click',function(e){
             if($(e.target).closest('.dismiss').length != 0){
                 $('body').css({'overflow':'auto'});
-                $target.addClass('fade').removeClass('in').css({'display':'none'});
+                $target.addClass('anim-fadeOut')//.removeClass('in').css({'display':'none'});
             }
         });
         $(document).on('click','.modal',function(e){
              if($(e.target).closest('.modal-content').length == 0){
                 $('body').css({'overflow':'auto'});
-                $target.addClass('fade').removeClass('in').css({'display':'none'});
+                $target.addClass('anim-fadeOut')//.removeClass('in').css({'display':'none'});
             }
         });
     }
