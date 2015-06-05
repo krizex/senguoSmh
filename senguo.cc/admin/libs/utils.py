6a01a785c6d2a4f4013e3123b64835c9dddaf9b8
Sky <littlesky87@gmail.com>
2015-06-05 16:24:35 +0800

    test

--- senguo.cc/admin/libs/utils.py
+++ senguo.cc/admin/libs/utils.py
@@ -1,10 +1,10 @@
 class Logger:
     @classmethod
     def warn(cls, title, detail=""):
-        print('\033[37m\033[43m'+'[警告]'+'\033[0m', title, detail)
+        print("[◉]", title, detail)
     @classmethod
     def error(cls, title, detail=""):
-        print('\033[37m\033[41m'+'[错误]'+'\033[0m', title, detail)
+        print("[✖︎]", title, detail)
     @classmethod
     def info(cls, title, detail=""):
-        print('\033[37m\033[46m'+'[信息]'+'\033[0m', title, detail)
+        print("[⚠]", title, detail)
