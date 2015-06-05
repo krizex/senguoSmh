6018b66027c77dce033f3a67da60f889f2beb96b
Sky <littlesky87@gmail.com>
2015-06-05 18:51:57 +0800

    run.app

--- senguo.cc/admin/run_app.sh
+++ senguo.cc/admin/run_app.sh
@@ -1,1 +1,2 @@
 nohup ./app.py -port=8887 2>&1 & nohup ./app.py -port=8888 2>&1 & nohup ./app.py -port=9090 2>&1 & nohup ./app.py -port=8886 2>&1 &
+nohup ./utils/db_init/order_auto_comment.py  2>&1 &
