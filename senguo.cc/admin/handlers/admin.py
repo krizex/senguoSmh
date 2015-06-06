6e122666dcfa4e82f67a3a96034b67586dfb7903
senguo2014 <senguo2014@163.com>
2015-06-06 12:10:16 +0800

    fix

--- senguo.cc/admin/handlers/admin.py
+++ senguo.cc/admin/handlers/admin.py
@@ -1687,7 +1687,7 @@ class Goods(AdminBaseHandler):
 						group_id = group_id,
 						detail_describe = data["detail_describe"]
 						)
-				return self.send_success(img_url=img_urls)
+				return self.send_success(imgurl=img_urls)
 
 			elif action == "default_goods_img":  # 恢复默认图
 				goods.img_url = ''
