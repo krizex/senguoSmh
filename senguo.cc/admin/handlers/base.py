071184bbf1c4c332afb144f85bb1999f1bade24f
senguo2014 <senguo2014@163.com>
2015-06-09 15:10:02 +0800

    fix 上传编辑器图片回调

--- senguo.cc/admin/handlers/base.py
+++ senguo.cc/admin/handlers/base.py
@@ -524,7 +524,7 @@ class _AccountBaseHandler(GlobalBaseHandler):
 
 
 		token = q.upload_token(BUCKET_SHOP_IMG, expires=60*30*100,
-							  policy={"returnUrl": "http://zone.senguo.cc/admin/editorCallback",
+							  policy={"returnUrl": "http://test123.senguo.cc/admin/editorCallback",
 									  "returnBody": "key=$(key)&action=%s&id=%s" % (action, id), "mimeLimit": "image/*"})
 		print("[七牛授权]发送Token：",token)
 		return self.send_success(token=token, key=action + ':' + str(time.time())+':'+str(id))
