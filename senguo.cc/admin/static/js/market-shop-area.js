13d25fcf6190e0cfa45b11748b33ab2c028d6c9b
Sky <littlesky87@gmail.com>
2015-06-17 14:12:55 +0800

    t

--- senguo.cc/admin/static/js/market-shop-area.js
+++ senguo.cc/admin/static/js/market-shop-area.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     }
     $(".wrap-area-box").height($(window).height());
     initBmap();
+    $(".BMap_pop").children("div").eq(0).children("div").css({"background":"#333","border-top-left-radius":"10px"});
+    $(".BMap_pop").children("div").eq(2).children("div").css({"background":"#333","border-top-right-radius":"10px"});
 }).on("click","#go-back",function(){
     history.go(-1);
 });
@@ -49,8 +51,6 @@ function initBmap() {
             }
         });
     }
-    $(".BMap_pop").children("div").eq(0).children("div").css({"background":"#333","border-top-left-radius":"10px"});
-    $(".BMap_pop").children("div").eq(1).children("div").css({"background":"#333","border-top-left-radius":"10px"});
 }
 
 
