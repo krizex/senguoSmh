dea0e9b1f81fa90c96cc2bfb3c404c6ba7082eb9
zhoubing <418650386@qq.com>
2015-06-19 10:22:43 +0800

    coupon

--- senguo.cc/admin/static/js/coupon.js
+++ senguo.cc/admin/static/js/coupon.js
@@ -4,4 +4,20 @@
 $(document).ready(function(){
     var height = $(window).height();
     $(".container").height(height);
+    //兑换
+    $(document).on("click","#convert-btn",function(){
+        var key = $("#convert-btn").val();
+        if($.trim(key)!=""){
+            searchKey(key);
+        }
+    });
+
 });
+function searchKey(id,key){
+    $.ajax({
+        url:"/coupon/detail?action=detail&coupon_key="+key,
+        type:"get",
+        success:function(res){
+        }
+    });
+}
