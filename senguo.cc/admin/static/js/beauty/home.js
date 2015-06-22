6502a42d6cb73384b80fa491e803bc6401c2c179
Sky <littlesky87@gmail.com>
2015-06-22 17:33:30 +0800

    图片宽度test

--- senguo.cc/admin/static/js/beauty/home.js
+++ senguo.cc/admin/static/js/beauty/home.js
@@ -296,10 +296,10 @@ var fruitItem=function(box,fruits,type){
         ori_img='/static/design_img/'+code+'.png';
     }else{
          if(type=='recommend'){
-            ori_img=img_url+'?imageView/1/w/800/h/200';
+            ori_img=img_url+'?imageView2/2/w/800';
          }
         else{
-            ori_img=img_url+'?imageView/1/w/800/h/600';
+            ori_img=img_url+'?imageView2/2/w/800';
         }
     }
     if(tag==2){
