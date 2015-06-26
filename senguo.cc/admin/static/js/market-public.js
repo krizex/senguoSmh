ada5af19db545ecab40084fcdddb5e90e0da79bb
Sky <littlesky87@gmail.com>
2015-06-26 20:50:51 +0800

    t

--- senguo.cc/admin/static/js/market-public.js
+++ senguo.cc/admin/static/js/market-public.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
         unitText($this,id);
     });
     $(document).on('click','#backTop',function(){
-        $(window).scrollTop(0);
+        $(window).animate({scrollTop:0},800);
         $('.little_pear').css("display","none");
     });
     //从cookie中提取数据
