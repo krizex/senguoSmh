399ebe76e787e36d669d6d56436ec07d05d64017
Sky <littlesky87@gmail.com>
2015-07-01 21:06:20 +0800

    t

--- senguo.cc/admin/static/js/ueditor/umeditor.config.js
+++ senguo.cc/admin/static/js/ueditor/umeditor.config.js
@@ -143,7 +143,7 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         ,toolbar:[
-            'undo redo | bold italic underline strikethrough | forecolor backcolor |',
+            'undo redo | bold italic underline | forecolor backcolor removeformat |',
             'insertorderedlist insertunorderedlist | paragraph fontfamily fontsize' ,
             '| justifyleft justifycenter justifyright justifyjustify |',
             'link unlink | emotion image video map',
