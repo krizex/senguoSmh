91eb54ea87958e38de7bd43c382343431220f575
zhoubing <418650386@qq.com>
2015-07-06 22:12:01 +0800

    shop

--- senguo.cc/admin/static/m-admin/js/shop-set.js
+++ senguo.cc/admin/static/m-admin/js/shop-set.js
@@ -4,15 +4,6 @@
 $(document).ready(function(){
     var height = $(window).height();
     $(".container").css("minHeight",height-40+"px");
-    $('.auths').each(function(){
-        var $this=$(this);
-        var auth=parseInt($this.attr('data-auth'));
-        if(auth == 1 || auth == 4){
-            $this.text('个人认证').removeClass('hide');
-        }else if(auth == 2 || auth == 3){
-            $this.text('企业认证').removeClass('hide');
-        }
-    });
 }).on("click","#qa",function(){
     $(this).addClass("hide");
     $(".pop-bwin").removeClass("hide");
