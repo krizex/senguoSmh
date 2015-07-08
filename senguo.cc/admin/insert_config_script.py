b32b170b82d4cf6020b6928b937836614d9f2214
senguo2014 <senguo2014@163.com>
2015-07-08 21:17:42 +0800

    spider add setaddress

--- senguo.cc/admin/insert_config_script.py
+++ senguo.cc/admin/insert_config_script.py
@@ -9,63 +9,13 @@ import requests
 import openpyxl
 from openpyxl import Workbook
 import datetime
+import json
 
 proxyURL_list=["http://119.131.157.176","http://117.147.233.140","http://117.84.182.12","http://112.112.136.233","http://175.184.155.204","http://182.40.51.145","http://175.184.161.61","http://115.228.54.176","http://117.173.203.63","http://117.168.49.72","http://115.228.63.65","http://223.151.80.139","http://175.184.163.101","http://175.175.93.113","http://223.67.169.185"]
 
-import socket
-import random
-import netifaces as ni
-
-true_socket = socket.socket
-
-# class getLocalIps:
-#     global ipList
-#     ipList=["119.131.157.176","117.147.233.140","117.84.182.12","112.112.136.233","175.184.155.204","182.40.51.145","175.184.161.61","115.228.54.176","117.173.203.63","117.168.49.72","115.228.63.65","223.151.80.139","175.184.163.101","175.175.93.113","223.67.169.185"]
-#     def getLocalEthIps(self):
-#         for dev in ni.interfaces():
-#             if dev.startswith('eth0'):
-#                 ip=ni.ifaddresses(dev)[2][0]['addr']
-#                 print(ip,111111)
-#                 if ip not in ipList:
-#                     ipList.append(ip)
-
-# class bindIp:
-#     ip=''
-#     global true_socket,ipList
-#     ipList=["119.131.157.176","117.147.233.140","117.84.182.12","112.112.136.233","175.184.155.204","182.40.51.145","175.184.161.61","115.228.54.176","117.173.203.63","117.168.49.72","115.228.63.65","223.151.80.139","175.184.163.101","175.175.93.113","223.67.169.185"]
-
-#     def bound_socket(self,*a, **k):
-#         sock = true_socket(*a, **k)
-#         print(6666)
-#         sock.bind((self.ip,0))
-#         return sock
-
-#     def changeIp(self,ipaddress):
-#         self.ip=ipaddress
-#         if not self.ip=='':
-#             socket.socket = self.bound_socket()
-#         else:
-#             socket.socket = true_socket
-
-#     def randomIp(self):
-#         if len(ipList)==0:
-#             getLocalIpsFunction=getLocalIps()
-#             getLocalIpsFunction.getLocalEthIps()
-#             if len(ipList)==0:
-#                 return
-#         _ip=random.choice(ipList)
-#         print(_ip,222222)
-#         if not _ip==self.ip:
-#             self.changeIp(_ip)
-
-#     def getIp(self):
-#         return self.ip
-
-#     def getIpsCount(self):
-#         return len(ipList)
-
-# bindIpObj= bindIp()
-# bindIpObj.randomIp()
+wb = Workbook()
+ws = wb.active
+ws.append(["店铺名","店铺地址","店铺电话","店铺公告","配送费","起送金额","配送时间","配送范围","店铺链接"])
 
 class Taobao:
     def __init__(self):
@@ -242,7 +192,13 @@ class Taobao:
 
     def getListPage(self,addressId):
         URL = 'http://waimai.taobao.com/shop_list.htm?spm=a221y.7437588.0.0.m0K9Ee&page=1&posy=11434388&posx=3055056&orderby=recommend&cateId=109&addressId='+addressId
-        shoplist_response = self.newOpener.open(URL)
+        headers = {
+            'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36',
+            'Host':'waimai.taobao.com',
+            'Connection' : 'Keep-Alive'
+        }
+        shoplist_request = urllib2.Request(URL,headers=headers)
+        shoplist_response = self.newOpener.open(shoplist_request)
         shoplist_result =  shoplist_response.read().decode('gbk')
         soup = BeautifulSoup(shoplist_result,"lxml",from_encoding="utf-8")
         page_item = soup.find("input",{"id":"J_page_count"})
@@ -256,9 +212,6 @@ class Taobao:
         page_count = self.getListPage(addressId)
         print(page_count,"iam list page")
         shop_info_list = []
-        wb = Workbook()
-        ws = wb.active
-        ws.append(["店铺名","店铺地址","店铺电话","店铺公告","配送费","起送金额","配送时间","配送范围","店铺链接"])
         for i in range(page_count):
             i=i+1
             shoplistURL = 'http://waimai.taobao.com/shop_list.htm?spm=a221y.7437588.0.0.m0K9Ee&page='+str(i)+'&posy=11434388&posx=3055056&orderby=recommend&cateId=109&addressId='+addressId
@@ -268,10 +221,8 @@ class Taobao:
                 'Host':'waimai.taobao.com',
                 'Connection' : 'Keep-Alive'
             }
-            proxies={
-                "http":'http://117.168.49.72'
-            }
-            shoplist_response = self.newOpener.open(shoplistURL)
+            shoplist_request = urllib2.Request(shoplistURL,headers=headers)
+            shoplist_response = self.newOpener.open(shoplist_request)
             shoplist_result =  shoplist_response.read().decode('gbk')
             status = shoplist_response.getcode()
 
@@ -301,7 +252,8 @@ class Taobao:
                 shopUrlList.append(shopURL)
                 # print(shopURL)
 
-                shop_response = self.newOpener.open(shopURL)
+                shop_request = urllib2.Request(shopURL,headers=headers)
+                shop_response = self.newOpener.open(shop_request)
                 shop_resrult =  shop_response.read().decode('gbk')
 
                 shop_soup = BeautifulSoup(shop_resrult,"lxml",from_encoding="utf-8")
@@ -380,6 +332,67 @@ class Taobao:
         print(shop_info_list)
         wb.save("sample.xlsx")
 
+    def setAddress(self):
+        wh_area=["上海街道","大智街道","一元街道","车站街道","四唯街道","永清街道","西马街道","球场街道","劳动街道","二七街道","新村街道","丹水池街道","台北街道","花桥街道","谌家矶街道","后湖街道","积玉桥街道","杨园街道","徐家棚街道","南湖街道","粮道街道","中华路街道","黄鹤楼街道","紫阳街道","白沙洲街道","首义路街道","中南路街道","水果湖街道","珞珈山街道","石洞街道",
+"民族街道","花楼街道","水塔街道","民权街道","满春街道","民意街道","新华街道","万松街道","唐家墩街道","北湖街道","前进街道","常青街道","汉兴街道",
+"易家墩街道","韩家墩街道","宗关街道","汉水桥街道","宝丰街道","荣华街道","崇仁街道","汉中街道","汉正街道","六角亭街道","长丰街道",
+"翠微街道","建桥街道","月湖街道","晴川街道","鹦鹉街道","洲头街道","五里墩街道","琴断口街道","江汉二桥街道","永丰街道","江堤街道",
+"红卫路街道",'钢花村街道','冶金街道',"新沟桥街道","红钢城街道","工人村街道","青山镇街道","厂前街道","武东街道","白玉山街道","钢都花园街道","北湖街道","武钢厂区",
+"珞南街道","关山街道","狮子山街道","张家湾街道","梨园街道","卓刀泉街道","洪山街道","和平街道","青菱街道","天兴乡",
+"吴家山街道","柏泉街道街道","将军路街道","慈惠街道","走马岭街道","径河街道","长青街道","辛安渡街道街道","东山街道街道","常青花园新区街道","新沟镇街道","金银湖街道",
+"纱帽街道","东荆街道","湘口街道","邓南街道","蔡甸街道","奓山街道","永安街道","侏儒街道","大集街道","张湾街道","玉贤镇","索河镇","消泗乡","桐湖农场",'洪北农业示范区',
+"纸坊街道","金口街道","乌龙泉街道","郑店街道","五里界街道","庙山街道","金水街道",'藏龙岛街道',"法泗镇","安山街道","湖泗镇","山坡乡","舒安乡","梁子湖风景区",
+"前川街道","祁家湾街道","横店街道","罗汉寺街道",'滠口街道','六指街道',"天河街道","武湖街道","姚家集镇","蔡家榨镇","王家河镇","李家集镇","长轩岭镇","三里镇","蔡店乡","木兰乡","木兰山风景管理处","大潭原种场","武汉盘龙城经济开发区","邾城街道","阳逻街道","仓埠街道","汪集街道","李集街道","三店街道","潘塘街道","旧街街道","双柳街道","涨渡湖街道","辛冲镇","徐古镇","凤凰镇","道观河风景旅游区","武汉阳逻经济开发区"]
+
+        for i in range(len(wh_area)):
+            area = wh_area[i]
+            print(area)
+            # headers = {
+            #     'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36',
+            #     'Host':'waimai.taobao.com',
+            #     'Connection' : 'Keep-Alive',
+            #     'Referer':'http://waimai.taobao.com/my_address.htm?spm=a221y.7437588.0.0.8PbTh3&city=420100'
+            # }
+            # maplink="http://waimai.taobao.com/asynJsonQuery.do?action=userAddressOperateAction&event_submit_do_getPosxyByAddress=true&cityname=武汉&address="+area
+            # map_request = urllib2.Request(maplink,headers=headers)
+            # map_response = self.newOpener.open(map_request)
+            # map_result =  map_response.read().decode('gbk')
+            # _tb_token_=TFy2vVmVDZzHCl9&city=420100&location=shopList&posx=%0D%0A30.55036&posy=114.30998%0D%0A&id=14387778&errorMsg=&targetUrl=&keyword=%CE%E4%BA%BA%CA%D0%C1%B8%B5%C0%BD%D6
+            url = "http://api.map.baidu.com/geocoder/v2/?address= 湖北省武汉市"+area+"&output=json&ak=2595684c343d6499bf469da8a9c18231"
+            r = requests.get(url)
+            result = json.loads(r.text)
+            if result["status"] == 0:
+                lat = result["result"]["location"]["lat"]
+                lon = result["result"]["location"]["lng"]
+            # print(lat,lon)
+
+            headers =  {
+                'Host':'waimai.taobao.com',
+                'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36',
+                'Referer' : 'http://waimai.taobao.com/my_address.htm',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Connection' : 'Keep-Alive'
+            }
+            change_link = "http://waimai.taobao.com/my_address.htm"
+            change_data={
+                "_tb_token_":"TFy2vVmVDZzHCl9",
+                "city":420100,
+                "location":"shopList",
+                "posx":lat,
+                "posy":lon,
+                "id":14388453,
+                "errorMsg":"",
+                "targetUrl":"",
+                "keyword":"%CE%E4%BA%BA%CA%D0%C1%B8%B5%C0%BD%D6"
+            }
+            data=urllib.urlencode(change_data)
+            change_request = urllib2.Request(change_link,data,headers)
+            change_response = self.newOpener.open(change_request)
+            change_content = change_response.read().decode('gbk')
+            print(change_content)
+            self.handleData('14388453')
+
+
     def main(self):
         needResult = self.needCheckCode()
         if not needResult ==None:
@@ -406,7 +419,8 @@ class Taobao:
         result = self.loginByST(st,self.username)
 
         if result:
-            self.handleData('14387897')
+            self.setAddress()
+            # self.handleData('14387897')
         else:
             print u"login fail"
 
