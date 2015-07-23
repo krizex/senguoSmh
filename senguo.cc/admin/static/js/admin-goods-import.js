09729dff170e5e409654578f4898997969cf56e9
senguo2014 <senguo2014@163.com>
2015-07-23 21:35:53 +0800

    goods data import

--- senguo.cc/admin/static/js/admin-goods-import.js
+++ senguo.cc/admin/static/js/admin-goods-import.js
@@ -5,6 +5,7 @@ var type = 0,page=0;
 $(document).ready(function(){
     $(".wrap-goods-groups").prev(".subnav-box").hide();
     $(".right-title").hide();
+    getGoods(parseInt($(".shop_name").attr("data-id")));
 }).on("click",".import-type li",function(){
     var index = $(this).index();
     type = index;
@@ -16,7 +17,7 @@ $(document).ready(function(){
         return false;
     }
     if(type==0){
-
+        importGoods($(this));
     }else{
 
     }
@@ -39,4 +40,76 @@ $(document).ready(function(){
         $(".shop-list li").removeClass("active");
         $("#choose_txt").html(0);
     }
+}).on("click",".shop_profile_list .item",function(){
+    var $this=$(this);
+    var id=parseInt($this.attr("data-id"));
+    $(".shop_name").attr("data-id",id).text($this.text());
+    getGoods(id);
 });
+var goods_item='<li data-id={{id}}>'+
+                    '<i class="checkbox-btn"></i>'+
+                    '<img class="shop-img" src="{{logo}}" alt="店铺logo"/>'+
+                    '<div class="shop-item">'+
+                        '<p class="c333" title="{{name}}">{{name}}</p>'+
+                        '{{ if charge_types }}<p class="c666">价格{{charge_types["price"]}}元/{{charge_types["unit"]}}</p>{{/if}}'+
+                    '</div>'+
+                '</li>';
+function getGoods(shop_id){
+    var args = {
+        action:"get_goods",
+        data:{
+            id:shop_id,
+        }
+    };
+    var url = "";
+    $(".shop-list").empty();
+    $.postJson(url,args,function(res){
+         if(res.success){
+             var goods_list=res.goods_list;
+             for(var key in goods_list){
+                var good=goods_list[key];
+                var render=template.compile(goods_item);
+                var html=render({
+                    logo:good["imgurl"],
+                    id:good["id"],
+                    name:good["name"],
+                    charge_types:good["charge_types"][0]
+                });
+                $(".shop-list").append(html);
+             }
+         }else{
+            return Tip(res.error_text);
+         }
+    });
+}   
+
+function importGoods($btn){
+    if($btn.attr("data-flag")=="off"){
+        return Tip("请勿重复提交");
+    }
+    var id_list=[];
+    $(".shop-list .active").each(function(){
+        var $this=$(this);
+        var id=parseInt($this.attr("data-id"));
+        id_list.push(id);
+    });
+    var url = "";
+    var args = {
+        action:"import_goods",
+        data:{
+            fruit_list:id_list,
+        }
+    };
+    $btn.attr("data-flag","off");
+    $.postJson(url,args,function(res){
+         if(res.success){
+            Tip("导入成功");
+             setTimeout(function(){
+                 window.location.href='/admin/goods/all';
+             },1500);
+         }else{
+            $btn.attr("data-flag","on");
+            return Tip(res.error_text);
+         }
+    });
+}
