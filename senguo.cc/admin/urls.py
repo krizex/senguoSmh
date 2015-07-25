ddb7f5ce5a8325b3fc89869ba10c41ba11396324
Sky <littlesky87@gmail.com>
2015-07-25 10:00:31 +0800

    f

--- senguo.cc/admin/urls.py
+++ senguo.cc/admin/urls.py
@@ -21,7 +21,7 @@ from dal import models
 # ]
 
 handlers = [
-	
+
 	#告白墙
 	(r"/lovewall/public/(\w+)", handlers.activity.ConfessionPublic, {}, "ConfessionPublic"),
 	(r"/lovewall/center/(\w+)", handlers.activity.ConfessionCenter, {}, "ConfessionCenter"),
@@ -48,7 +48,7 @@ handlers = [
 	#优惠券
 	(r"/coupon", handlers.activity.Coupon, {}, "Coupon"),
 	(r"/coupon/detail", handlers.activity.CouponDetail, {}, "CouponDetail"),
-	
+
 	(r"/staff/login", handlers.staff.Access, {"action":"login"}, "staffLogin"),
 	(r"/staff/oauth", handlers.staff.Access, {"action":"oauth"}, "staffOauth"),
 	(r"/staff/logout", handlers.staff.Access, {"action":"logout"}, "staffLogout"),
@@ -65,7 +65,7 @@ handlers = [
 	(r"/customer/weixin", handlers.customer.Third, {"action":"weixin"}, "customerWeixin"),
 	(r"/customer/register", handlers.customer.RegistByPhone, {}, "customerRegister"),
 	(r"/customer/password", handlers.customer.Password, {}, "customerPassword"),
-	
+
 	(r"/customer/profile", handlers.customer.CustomerProfile, {}, "customerProfile"),
 	(r"/customer/wxauth", handlers.customer.WxBind, {"action":"wx_auth"}, "customerwxAuth"),
 	(r"/customer/wxBind", handlers.customer.WxBind, {"action":"wx_bind"}, "customerwxBind"),
@@ -125,8 +125,8 @@ handlers = [
 	(r"/customer/(\w+)", handlers.customer.Home, {}, "customerHome"),
 	#店铺地图
 	(r"/shoparea/(\w+)",handlers.customer.ShopArea,{},"shoparea"),
-	
-	
+
+
 	(r"/super/oauth", handlers.superadmin.Access,{
 		"action":"oauth"}, "superOauth"),
 	(r"/super/logout", handlers.superadmin.Access,{
@@ -206,7 +206,7 @@ handlers = [
 
 
 	# (r"/super/notice/", handlers.superadmin.Notice),
-	
+
 	(r"/admin/login", handlers.admin.Access,{"action":"login"}, "adminLogin"),
 	(r"/admin/oauth", handlers.admin.Access, {"action":"oauth"}, "adminOauth"),
 	(r"/admin/logout", handlers.admin.Access, {"action":"logout"}, "adminLogout"),
@@ -246,7 +246,7 @@ handlers = [
 	(r"/admin/editorCallback", handlers.admin.editorCallback, {}, "admineditorCallback"),
 	(r"/admin/MessageManage", handlers.admin.MessageManage, {}, "adminMessageManage"),
 	(r"/admin/WirelessPrint", handlers.admin.WirelessPrint, {}, "WirelessPrint"),
-	
+
 	# (r"/admin/customer", handlers.admin.Customer, {}, "adminCustomer"),
 	# (r"/admin/staff", handlers.admin.Staff, {}, "adminStaff"),
 	# (r"/admin/finance", handlers.admin.Finance, {}, "adminFinance"),
@@ -264,7 +264,7 @@ handlers = [
 	(r"/madmin/shopaddress", handlers.madmin.Address, {}, "MadminAddress"),
 	(r"/madmin/shopattr", handlers.madmin.SetAttr, {}, "MadminSetAttr"),
 
-	
+
 	# 水果圈子
 
 	# 主页
@@ -276,7 +276,7 @@ handlers = [
 	(r"/fruitzone/admin/profile", handlers.fruitzone.AdminProfile, {}, "fruitzoneAdminProfile"),
 	(r"/fruitzone/paytest",handlers.customer.payTest,{},"fruitzonePayTest"),
 	(r"/fruitzone/searchlist",handlers.fruitzone.SearchList,{},"searchlist"),
-	#to remove  
+	#to remove
 	#woody
 	(r"/apply/toweixin", handlers.fruitzone.ToWeixin, {}, "fruitzoneToWexin"),
 	# (r"/fruitzone/apply", handlers.fruitzone.ShopApply, {"action": "apply"}, "fruitzoneShopApply"),
@@ -305,7 +305,7 @@ handlers = [
 
 	(r"/fruitzone/phoneVerify", handlers.fruitzone.PhoneVerify, {
 		"action":"admin"}, "fruitzonePhoneVerify"),
-	
+
 
 	#信息墙
 	(r"/infowall/supply", handlers.infowall.Home, {"action": "supply"}, "infowallHomeSupply"),
@@ -350,7 +350,5 @@ handlers = [
 	(r"/fruitzone/imgcallback", handlers.fruitzone.QiniuCallback, {"action":"shop" }, "imgCallback"),
 	(r"/fruitzone/shopImgCallback", handlers.fruitzone.QiniuCallback, {"action": "edit_shop_img"}, "fruitzoneshopImgCallback"),
 	(r"/fruitzone/InfoImgCallback", handlers.fruitzone.QiniuCallback, {"action": "edit_info_img"}, "fruitzoneInfoImgCallback"),
-	(r"/admin/shelf/fruitImgCallback", handlers.fruitzone.QiniuCallback,
-	 {"action": "edit_fruit_img"}, "adminShelfFruitImgCallback"),
+	(r"/admin/shelf/fruitImgCallback", handlers.fruitzone.QiniuCallback, {"action": "edit_fruit_img"}, "adminShelfFruitImgCallback"),
 ]
-
