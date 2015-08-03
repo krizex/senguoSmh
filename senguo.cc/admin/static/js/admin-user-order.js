2cfcc57244c3a2ec0f89dfa17153a9e4e6ce4f93
Sky <littlesky87@gmail.com>
2015-08-03 10:49:31 +0800

    fix sth

--- senguo.cc/admin/static/js/admin-user-order.js
+++ senguo.cc/admin/static/js/admin-user-order.js
@@ -951,7 +951,7 @@ function initBmap(){
             mark.setAnimation(BMAP_ANIMATION_BOUNCE);
             setTimeout(function(){
                 mark.setAnimation();
-            },3000);
+            },2000);
         }else{
             return Tip("该自提点未找到");
         }
