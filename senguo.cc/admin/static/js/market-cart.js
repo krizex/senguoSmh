545e3d7725cc12af4c1e78bb351110a981a56ca4
senguo2014 <senguo2014@163.com>
2015-08-06 11:45:47 +0800

    fix

--- senguo.cc/admin/static/js/market-cart.js
+++ senguo.cc/admin/static/js/market-cart.js
@@ -496,17 +496,18 @@ function todayChoose(){
                 var stop_now_time=$(".now_startMin").val();
                 var stop_now=parseInt($(".now_stop").val());
                 var _time_now;
+                var _time_now_real=checkTime(_time.getHours())+':'+checkTime(_time.getMinutes())+':'+checkTime(_time.getSeconds());
                 if(_time.getMinutes()+stop_now>=60){
                     _time_now=checkTime(_time.getHours()+1)+':'+checkTime(_time.getMinutes())+':'+checkTime(_time.getSeconds());
                 }else{
-                    _time_now=checkTime(_time.getHours())+':'+checkTime(_time.getMinutes())+':'+checkTime(_time.getSeconds());
+                    _time_now=_time_now_real;
 
                 }
                 console.log(stop_now_time);
                 console.log(start_now_time);
                 console.log(_time_now);
                 console.log(stop_now_time>_time_now);
-                if(stop_now_time>_time_now&&_time_now>start_now_time){
+                if(stop_now_time>_time_now&&_time_now_real>start_now_time){
                     $(".send-now").show().addClass("active").addClass("available");
                     minNow();
                     freightNow();
