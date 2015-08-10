151c006ac7b39f4ed8d3a3b700353742d690edc8
dnxbjyj <dnxbjyj@126.com>
2015-08-10 12:37:47 +0800

    bug on superadmin user

--- senguo.cc/admin/static/js/superadmin-user.js
+++ senguo.cc/admin/static/js/superadmin-user.js
@@ -198,9 +198,14 @@ function getSearchContent(action,inputinfo,page){
                     var city=user[5];
 
                     var phone=user[6];
+                    var wx_username=user[8];
+                    if(!wx_username){
+                        wx_username = "未填写";
+                    }
+                    $item.find('.wx-username').text(wx_username);
 
                     //add by jyj 2015-6-22
-                    var birthday = user[8];
+                    var birthday = user[9];
                     if(birthday == 0){
                             birthday = "未填写";
                     }
@@ -209,7 +214,7 @@ function getSearchContent(action,inputinfo,page){
                     //
 
                     //change by jyj 2015-6-24
-                    var fshop=user[9];
+                    var fshop=user[10];
                     if(fshop.length != 0){
                         if(fshop[0][2].length != 0){
                             for(j= 0;j< fshop.length;j++){
@@ -227,7 +232,7 @@ function getSearchContent(action,inputinfo,page){
                     }
                     //
 
-                    var oshop=user[10];
+                    var oshop=user[11];
 
                     if(!phone) phone='未绑定';
                     $item.find('.img').attr({'src':img});
