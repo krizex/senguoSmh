43b0bfc5febb45784bde2e4f45d89e88430739c4
dnxbjyj <dnxbjyj@126.com>
2015-08-12 09:42:27 +0800

    admin seckill

--- senguo.cc/admin/static/js/admin-marketing-seckill.js
+++ senguo.cc/admin/static/js/admin-marketing-seckill.js
@@ -1,16 +1,26 @@
+
+var switch_status = 
 $(document).ready(function(){
 
-}).on('click', '.confess-only', function () {
+}).on('click', '.open-switch', function () {
     	var $this = $(this);
-    	if ($this.attr("data-flag") == "off") return false;
-    	$this.attr("data-flag", "off");
-    	 var status = Int($this.attr('data-status'));
-    	 if (status == 1) {
-	                    $this.attr({'data-status': 0}).addClass('stop-mode').removeClass('work-mode').find('.tit').text('未启用');
-	  }
-              else if (status == 0) {
-                  $this.attr({'data-status': 1}).removeClass('stop-mode').addClass('work-mode').find('.tit').text('已启用');
-              }
+    	var status = parseInt($this.attr("data-status"));
+    	if (status == 1){
+    		$this.attr({'data-status': 0}).addClass('stop-mode').removeClass('work-mode').find('.tit').text('未启用');
+    	}
+    	else if (status ==0) {
+    		$this.attr({'data-status': 1}).removeClass('stop-mode').addClass('work-mode').find('.tit').text('已启用');
+    	}
+
+   //  	if ($this.attr("data-flag") == "off") return false;
+   //  	$this.attr("data-flag", "off");
+   //  	 var status = Int($this.attr('data-status'));
+   //  	 if (status == 1) {
+	  //                   $this.attr({'data-status': 0}).addClass('stop-mode').removeClass('work-mode').find('.tit').text('未启用');
+	  // }
+   //            else if (status == 0) {
+   //                $this.attr({'data-status': 1}).removeClass('stop-mode').addClass('work-mode').find('.tit').text('已启用');
+   //            }
 	    // var status = Int($this.attr('data-status'));
 	    // var url = '';
 	    // var action = "confess_only";
