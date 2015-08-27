ff184cba4c6a40885f236b05ae882c3eb5b8bcc4
zhoubing <418650386@qq.com>
2015-08-27 21:43:13 +0800

    update

--- senguo.cc/admin/static/js/official_product.js
+++ senguo.cc/admin/static/js/official_product.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
     timer=setInterval(function(){
         slide_images();
     },4000);
+    if(navigator.userAgent.toLowerCase().indexOf("8.0")!=-1){//IE8
+        $(".big-roll").addClass("hidden");
+    }else if(navigator.userAgent.toLowerCase().indexOf("9.0")!=-1){
+        $(".big-roll").css("display","block");
+    }else if(navigator.userAgent.toLowerCase().indexOf("10.0")!=-1){
+        $(".big-roll").css("display","block");
+    }
 });
 function slide_images(){
     $(".slide-list li").each(function(){
