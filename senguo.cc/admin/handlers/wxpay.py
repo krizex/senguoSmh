3deaa794bade51854741a394e0de9dfd62b107d4
wdg8106 <1499728038@qq.com>
2015-09-02 20:02:36 +0800

    将wxpay引用的appID与setting文件一致

--- senguo.cc/admin/handlers/wxpay.py
+++ senguo.cc/admin/handlers/wxpay.py
@@ -47,15 +47,16 @@ try:
 except ImportError:
     pycurl = None
 
+from settng import MP_APPID, MP_APPSECRET
 
 class WxPayConf_pub(object):
     """配置账号信息"""
 
     #=======【基本信息设置】=====================================
     #微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
-    APPID = "wx0ed17cdc9020a96e"
+    APPID = MP_APPID #   "wx0ed17cdc9020a96e"
     #JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
-    APPSECRET = "cef2c4c6a59e6d0ae8b5a3f741a9c788"
+    APPSECRET = MP_APPSECRET  # "cef2c4c6a59e6d0ae8b5a3f741a9c788"
     #受理商ID，身份标识
     MCHID = "10023430"
     #商户支付密钥Key。审核通过后，在微信发送的邮件中查看
