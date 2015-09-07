9b7aca1fc52222a9bbb64c5d2b8035916aee668b
dnxbjyj <dnxbjyj@126.com>
2015-09-07 17:46:25 +0800

    seckill debug jyj

--- senguo.cc/admin/handlers/customer.py
+++ senguo.cc/admin/handlers/customer.py
@@ -431,6 +431,8 @@ class customerGoods(CustomerBaseHandler):
 				seckill_price_dif = 0
 				seckill_activity_piece = 0
 				is_seckill = 0
+				former_charge_type = None
+				seckill_not_start = 0
 
 				if has_discount_activity == 0:
 					self.update_seckill()
@@ -449,17 +451,25 @@ class customerGoods(CustomerBaseHandler):
 
 						time_query = self.session.query(models.SeckillActivity).join(models.SeckillGoods,models.SeckillGoods.activity_id == models.SeckillActivity.id).filter(models.SeckillGoods.id == seckill_goods.id).first()
 						seckill_start_time = time_query.start_time
+
 						end_time = time_query.end_time
 
 						seckill_price_dif = round(float(seckill_goods.former_price - seckill_goods.seckill_price),2)
 						if seckill_goods.activity_piece - seckill_goods.ordered > 0:
 							seckill_activity_piece= seckill_goods.activity_piece - seckill_goods.ordered
 
-				charge_types.append({'id':charge_type.id,'price':round(charge_type.price*discount_rate/10,2),'num':charge_type.num, 'unit':unit,\
-					'market_price':charge_type.market_price,'relate':charge_type.relate,"limit_today":limit_today,"allow_num":allow_num,\
+						if seckill_start_time < int(time.time()):
+							has_activity = 0
+							seckill_not_start = 1
+							former_charge_type = self.session.query(modes.ChargeType).filter_by(id = seckill_goods.charge_type_id).first()
+							unit = self.getUnit(former_charge_type.unit)
+				charge_types.append({'id':former_charge_type.id if seckill_not_start else charge_type.id,'price':round(former_charge_type.price,2) if seckill_not_start else round(charge_type.price*discount_rate/10,2),\
+					'num':former_charge_type.num if seckill_not_start else charge_type.num, 'unit':unit,\
+					'market_price':former_charge_type.market_price if seckill_not_start else charge_type.market_price,'relate':former_charge_type.relate if seckill_not_start else charge_type.relate,"limit_today":limit_today,"allow_num":allow_num,\
 					"has_activity":has_activity,"discount_rate":discount_rate,"is_seckill":is_seckill,\
 					"seckill_is_bought":seckill_is_bought,"seckill_activity_id":seckill_activity_id,"seckill_goods_id":seckill_goods_id,\
 					"seckill_price_dif":seckill_price_dif,"seckill_activity_piece":seckill_activity_piece})
+				
 		# added by jyj 9.3
 		seckill_goods_ids = []
 		
@@ -498,7 +508,6 @@ class customerGoods(CustomerBaseHandler):
 			cart_fs = [(key, cart_f[key]['num']) for key in cart_f if key not in key_allow]
 		cart_count = len(cart_f)
 		self.set_cookie("cart_count", str(cart_count))
-		print("###charge_types",charge_types)
 		return self.render('customer/goods-detail.html',good=good,img_url=img_url,has_activity=has_activity,end_time=end_time,shop_name=shop_name,charge_types=charge_types,cart_fs=cart_fs,\
 								seckill_goods_ids=seckill_goods_ids)
 
