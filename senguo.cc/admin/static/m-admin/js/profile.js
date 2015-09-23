feeba41fb8ed11a74c46b65de9429452f1340b8d
zhoubing <418650386@qq.com>
2015-09-23 15:12:01 +0800

    mbbs

--- senguo.cc/admin/static/m-admin/js/profile.js
+++ senguo.cc/admin/static/m-admin/js/profile.js
@@ -21,33 +21,10 @@ $(document).ready(function(){
             $(".wrap-menu-list").addClass("h0");
         }
     });
-    $("#bbs-menu").on("click",function(){
-        $(".wrap-menu-list").toggleClass("h0");
-        if($(".wrap-menu-list").hasClass("h0")){
-            $(this).removeClass("bbs-menu-at");
-        }else{
-            $(this).addClass("bbs-menu-at");
-        }
-    });
-    if(cookie.getCookie("mBbs")){
-        $(".tab-bm-list li").removeClass("active").eq(1).addClass("active");
-        $(".head_tab").addClass("hide").eq(1).removeClass("hide");
-        $(".main_tab").addClass("hide").eq(1).removeClass("hide");
-    }
 }).on("click","#shop_info",function(e){
     if($(e.target).closest(".shop-link").size()==0){
         window.location.href="/madmin/shopinfo";
     }  
-}).on("click",".tab-bm-list li",function(){
-    var index = $(this).index();
-    $(".tab-bm-list li").removeClass("active").eq(index).addClass("active");
-    $(".head_tab").addClass("hide").eq(index).removeClass("hide");
-    $(".main_tab").addClass("hide").eq(index).removeClass("hide");
-    if(index==1){
-        cookie.setCookie("mBbs","1");
-    }else{
-        cookie.removeCookie("mBbs");
-    }
 }).on("click",".order-links",function(){
     if($(this).hasClass("forbid-in")){
         Tip("该方式处于关闭状态，请开启后再查看");
@@ -55,13 +32,4 @@ $(document).ready(function(){
     }else{
         window.location.href=$(this).attr("url");
     }
-}).on("click","#search_btn",function(){
-    var key = $.trim($("#search_bbs").val());
-    if(key==""){
-        return Tip("请输入关键字");
-    }else{
-        window.location.href="/bbs?search="+key;
-    }
-}).on("click",".cancel-btn",function(){
-    $(".pop-win").addClass("hide");
 });
